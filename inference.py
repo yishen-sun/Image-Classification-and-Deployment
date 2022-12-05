dbb5a7db44b440c46a5e37a555a4bfb6e3dbd09f
yishen <yishen@node0.yishen-142021.uwmadison744-f22-pg0.utah.cloudlab.us>
2022-12-04 20:08:27 -0700

    test

--- inference.py
+++ inference.py
@@ -1,3 +1,14 @@
 from src.inference import Inference
+from flask import Flask, request, jsonify
+
+# Load the model
 infer = Inference()
-print(infer("data/tmp_1903.jpg"))
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def predict():
+    return infer("data/tmp_1903.jpg")
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
