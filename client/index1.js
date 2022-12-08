f7960d9f93d2db5a7a6bbb20c2445c31bc0e5b2b
yishen <yishen@node0.yishen-142021.uwmadison744-f22-pg0.utah.cloudlab.us>
2022-12-07 21:39:56 -0700

    adjust

--- client/index1.js
+++ client/index1.js
@@ -1,31 +1,6 @@
 
 url = "http://128.110.219.22:5000/api";
-// console.log(url);
-//   const image_input = document.querySelector("#image-input");
-//   var display_image = document.querySelector("#display-image");
-//   image_input.addEventListener("change", function () {
-//     const reader = new FileReader();
-//     reader.readAsDataURL(this.files[0]);
-//     reader.onload = function (e) {
-//       var xhr = new XMLHttpRequest();
-//       xhr.open("POST", url, true);
-//       xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-//       form_data = new FormData();
-//       form_data.append("base64", this.result);
-//       console.log(form_data);
-//       xhr.send(form_data);
-  
-//       xhr.onload = function () {
-//         console.log(this.responseText);
-//         //var data = JSON.parse(this.responseText);
-//         //console.log(data);
-//       };
-  
-//       //console.log(e);
-//       //console.log(this.result);
-//       display_image.setAttribute("src", this.result);
-//     };
-//   });
+
 const image_input = document.querySelector("#image-input");
 var display_image = document.querySelector("#display-image");
 var result = document.querySelector("#result");
@@ -56,5 +31,5 @@ function uploadFile(form) {
   oReq.onload = function () {
     result.innerHTML = "Result is " + this.responseText;   
   };
-  //display_image.setAttribute("src");
+
 }
--- server/inference.py
+++ server/inference.py
@@ -15,27 +15,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def predict():
     f = request.files['static_file']
     f.save(os.path.join("./data", f.filename))
-    
-    #data = request.form["base64"]
-    # print(request)
-    
-    #imgdata = base64.b64decode(data)
-    #file = open('/app/data/test.jpg','wb')
-    #file.write(data)
-    #file.close()
-    # print(os.getcwd())
-    # files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    # for f in files:
-    #     print(f)
     predict = infer(os.path.join("./data", f.filename))
-    #print(predict)
     return predict, 200
-    #print(data)
-    
-    #response.headers.add('Access-Control-Allow-Origin', '*')
-    #return "responsed!"
-    #resp = {"success": True, "response": "file saved!"}
-    #return jsonify(resp), 200
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
